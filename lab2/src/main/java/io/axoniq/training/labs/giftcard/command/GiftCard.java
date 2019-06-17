ffe11179a4636b9e57499b29a7a8e94e44f4f5b6
Peter Kullmann <p.kullmann@arenae.ch>
2019-06-17 13:54:57 +0200

    Lab 2

--- lab2/src/main/java/io/axoniq/training/labs/giftcard/command/GiftCard.java
+++ lab2/src/main/java/io/axoniq/training/labs/giftcard/command/GiftCard.java
@@ -1,5 +1,52 @@
 package io.axoniq.training.labs.giftcard.command;
 
+import io.axoniq.training.labs.giftcard.coreapi.CardIssuedEvent;
+import io.axoniq.training.labs.giftcard.coreapi.CardRedeemedEvent;
+import io.axoniq.training.labs.giftcard.coreapi.IssueCardCommand;
+import io.axoniq.training.labs.giftcard.coreapi.RedeemCardCommand;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import static org.axonframework.modelling.command.AggregateLifecycle.apply;
+
+@Aggregate
 public class GiftCard {
 
+    @AggregateIdentifier
+    private String cardId;
+
+    private int amountAvailable;
+
+    @EventSourcingHandler
+    public void on(CardIssuedEvent e) {
+        this.cardId = e.getCardId();
+        this.amountAvailable = e.getAmount();
+    }
+
+    @EventSourcingHandler
+    public void on(CardRedeemedEvent e) {
+        this.amountAvailable -= e.getAmount();
+    }
+
+
+    public GiftCard() {
+    }
+
+    @CommandHandler
+    public GiftCard(IssueCardCommand e) {
+        apply(new CardIssuedEvent(e.getCardId(), e.getAmount()));
+    }
+
+    @CommandHandler
+    public void handle(RedeemCardCommand e) {
+        if (amountAvailable < e.getAmount()) {
+            throw new IllegalStateException("Amount is too big");
+        }
+        if (e.getAmount() <= 0) {
+            throw new IllegalArgumentException("bla");
+        }
+        apply(new CardRedeemedEvent(e.getCardId(), e.getTransactionId(), e.getAmount()));
+    }
 }
--- lab2/src/test/java/io/axoniq/training/labs/giftcard/command/GiftCardTest.java
+++ lab2/src/test/java/io/axoniq/training/labs/giftcard/command/GiftCardTest.java
@@ -1,11 +1,13 @@
 package io.axoniq.training.labs.giftcard.command;
 
+import io.axoniq.training.labs.giftcard.coreapi.CardIssuedEvent;
+import io.axoniq.training.labs.giftcard.coreapi.CardRedeemedEvent;
+import io.axoniq.training.labs.giftcard.coreapi.IssueCardCommand;
+import io.axoniq.training.labs.giftcard.coreapi.RedeemCardCommand;
 import org.axonframework.test.aggregate.AggregateTestFixture;
 import org.axonframework.test.aggregate.FixtureConfiguration;
 import org.junit.Before;
 import org.junit.Test;
-
-import static org.junit.Assert.fail;
 
 public class GiftCardTest {
 
@@ -18,21 +20,30 @@ public class GiftCardTest {
 
     @Test
     public void shouldIssueGiftCard() {
-        fail("To be implemented");
+        fixture.givenNoPriorActivity()
+                .when(new IssueCardCommand("id", 12))
+                .expectEvents(new CardIssuedEvent("id", 12));
     }
 
     @Test
     public void shouldRedeemGiftCard() {
-        fail("To be implemented");
+        fixture.given(new CardIssuedEvent("id", 100))
+                .when(new RedeemCardCommand("id", "trans", 66))
+                .expectEvents(new CardRedeemedEvent("id", "trans", 66));
     }
 
     @Test
     public void shouldNotRedeemWithNegativeAmount() {
-        fail("To be implemented");
+        fixture.given(new CardIssuedEvent("id", 100))
+                .when(new RedeemCardCommand("id", "trans", -12))
+                .expectException(IllegalArgumentException.class);
     }
 
     @Test
     public void shouldNotRedeemWhenThereIsNotEnoughMoney() {
-        fail("To be implemented");
+        fixture.given(new CardIssuedEvent("id", 100), new CardRedeemedEvent("id", "t1", 66))
+                .when(new RedeemCardCommand("id", "t2", 50))
+                .expectException(IllegalStateException.class);
+
     }
 }
