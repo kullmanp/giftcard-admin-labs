ba9275d223701293a98ced75839f98a40fc31c95
Peter Kullmann <p.kullmann@arenae.ch>
2019-06-17 15:05:11 +0200

    Lab 3

--- lab3/src/main/java/io/axoniq/training/labs/giftcard/coreapi/CardSummary.java
+++ lab3/src/main/java/io/axoniq/training/labs/giftcard/coreapi/CardSummary.java
@@ -1,22 +1,50 @@
 package io.axoniq.training.labs.giftcard.coreapi;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.time.Instant;
 
+@Entity
 public class CardSummary {
 
+    @Id
+    private String cardId;
+
+    private int initialValue;
+    private Instant issuedAt;
+    private int remainingValue;
+
+    public CardSummary() {
+    }
+
+    public CardSummary(String cardId, int initialValue) {
+        this.cardId = cardId;
+        this.initialValue = initialValue;
+        this.remainingValue = initialValue;
+        this.issuedAt = Instant.now();
+    }
+
     public String getCardId() {
-        return null;
+        return cardId;
     }
 
     public int getInitialValue() {
-        return 0;
+        return initialValue;
     }
 
     public Instant getIssuedAt() {
-        return Instant.now();
+        return issuedAt;
     }
 
     public int getRemainingValue() {
-        return 0;
+        return remainingValue;
+    }
+
+    public void reimburse(int amount) {
+        this.remainingValue += amount;
+    }
+
+    public void redeem(int amount) {
+        this.remainingValue -= amount;
     }
 }
--- lab3/src/main/java/io/axoniq/training/labs/giftcard/query/CardSummaryProjection.java
+++ lab3/src/main/java/io/axoniq/training/labs/giftcard/query/CardSummaryProjection.java
@@ -1,8 +1,9 @@
 package io.axoniq.training.labs.giftcard.query;
 
-import io.axoniq.training.labs.giftcard.coreapi.CardSummary;
-import io.axoniq.training.labs.giftcard.coreapi.CountCardSummariesQuery;
-import io.axoniq.training.labs.giftcard.coreapi.FindCardSummariesQuery;
+import io.axoniq.training.labs.giftcard.command.GiftCardTransaction;
+import io.axoniq.training.labs.giftcard.coreapi.*;
+import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventsourcing.EventSourcingHandler;
 import org.axonframework.queryhandling.QueryHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,11 +25,36 @@ public class CardSummaryProjection {
 
     @QueryHandler
     public List<CardSummary> handle(FindCardSummariesQuery query) {
-        return Collections.emptyList();
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        return findAll().subList(query.getOffset(), query.getOffset() + query.getLimit());
+    }
+
+    private List<CardSummary> findAll() {
+        return entityManager.createQuery("select s from CardSummary s", CardSummary.class).getResultList();
     }
 
     @QueryHandler
     public Long handle(CountCardSummariesQuery query) {
-        return 0L;
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        return Long.valueOf(findAll().size());
     }
+
+    @EventHandler
+    public void cardIssued(CardIssuedEvent evt) {
+        CardSummary cardSummary = new CardSummary(evt.getCardId(), evt.getAmount());
+        entityManager.persist(cardSummary);
+    }
+
+    @EventHandler
+    protected void on(CardReimbursedEvent event) {
+        CardSummary cardSummary = entityManager.find(CardSummary.class, event.getCardId());
+        cardSummary.reimburse(event.getAmount());
+    }
+
+    @EventHandler
+    public void on(CardRedeemedEvent evt) {
+        CardSummary cardSummary = entityManager.find(CardSummary.class, evt.getCardId());
+        cardSummary.redeem(evt.getAmount());
+    }
+
 }
